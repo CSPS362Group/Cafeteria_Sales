536a074514886403e9184484f563a892b591d9c9
githubaccountNM <NEIDYMLG@GMAIL.COM>
2024-03-06 21:06:24 -0800

    Updated features
    I got the API for google and Paypal,
    All the code I got for Paypal Api can be found on Paypal's site
    
    I still haven't figured out how to get both APIs to work, I'm to the part where I can load checkout.html but cant put anything in paypal and I also need to decode the JWST from Google email

---
+++ script.js
@@ -0,0 +1,18 @@
+//document.getElementById('g-signin2').addEventListener('c')
+
+/*
+function init(){
+    gapi.load('auth2', function()){
+
+    }
+
+}*/
+
+function handleCredentialResponse(){
+
+}
+
+const button = document.getElementById('signout_button');
+button.onclick = () => {
+   /* google.accounts.id.disableAutoSelect();*/
+}
